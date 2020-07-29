b6866daaf20a3fc8ded74b6b7ca8ecd3d54fcc32
Andi Fauzy Dewantara <andifauzy00@gmail.com>
2020-07-29 09:46:59 +0700

    List User from API. GET

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get_api/details.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
+
 
 class HomePage extends StatefulWidget {
   @override
@@ -14,7 +16,7 @@ class _HomePageState extends State {
 
   Future<String> getData() async{
     http.Response response = await http.get(
-        Uri.encodeFull("https://jsonplaceholder.typicode.com/posts"),
+        Uri.encodeFull("http://api.bengkelrobot.net:8001/api/profile"),
         headers: {
           "Accept":"application/json"
         }
@@ -26,6 +28,7 @@ class _HomePageState extends State {
   }
 
   @override
+  // ignore: must_call_super
   void initState(){
     this.getData();
   }
@@ -34,14 +37,44 @@ class _HomePageState extends State {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("Data from API 2"),
+        leading: Icon(Icons.verified_user),
+        title: new Text("List User"),
         backgroundColor: Colors.blueAccent,
       ),
       body: new ListView.builder(
         itemCount: data == null ? 0 : data.length,
         itemBuilder: (BuildContext context, int index){
-          return new Card(
-            child: new Text(data[index]["title"]),
+          return InkWell(
+            onTap: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context) => Detail()));
+            },
+            child: new Card(
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.all(8.0),
+                        child: CircleAvatar(
+                          backgroundImage: NetworkImage("https://png.pngtree.com/png-vector/20190321/ourmid/pngtree-vector-users-icon-png-image_856952.jpg"),
+                          backgroundColor: Colors.transparent,
+                          radius: 40.0,
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text("ID : " + data[index]["id"].toString()),
+                          Text("Username : " + data[index]["name"]),
+                          Text("Email : " + data[index]["email"]),
+                          Text("Age : " + data[index]["age"].toString()),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           );
         },
       ),
